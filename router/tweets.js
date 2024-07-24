fa749089a2db8aacf740178c66c476e7cfabb2df
Seoyul Kim <zoeyul07@gmail.com>
2024-07-24 16:31:12 +0900

    refactor: controller 분리

--- router/tweets.js
+++ router/tweets.js
@@ -1,57 +1,23 @@
 import express from "express";
 import "express-async-errors";
-import * as tweetRepository from "../data/tweet.js";
+import * as tweetController from "../controller/tweet.js";
 
 const router = express.Router();
 
 //GET /tweets
 //GET /tweets?username=:username
-router.get("/", (req, res, next) => {
-  const userName = req.query.userName;
-  const data = userName
-    ? tweetRepository.getAllByUserName(userName)
-    : tweetRepository.getAll();
-
-  res.status(200).json(data);
-});
+router.get("/", tweetController.getTweets);
 
 //GET /tweets/:id
-router.get("/:id", (req, res, next) => {
-  const id = req.params.id;
-  const tweet = tweetRepository.getById(id);
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `tweet id:${id} not found` });
-  }
-});
+router.get("/:id", tweetController.getTweet);
 
 //POST /tweets
-router.post("/", (req, res, next) => {
-  const { text, name, userName } = req.body;
-  const tweet = tweetRepository.create(text, name, userName);
-  res.status(201).json(tweet);
-});
+router.post("/", tweetController.createTweet);
 
 //PUT /tweets/:id
-router.put("/:id", (req, res, next) => {
-  const id = req.params.id;
-  const text = req.body.text;
-
-  const tweet = tweetRepository.update(id, text);
-
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `tweet id:${id} not found` });
-  }
-});
+router.put("/:id", tweetController.updateTweet);
 
 //DELETE /tweets/:id
-router.delete("/:id", (req, res, next) => {
-  const id = req.param.id;
-  tweetRepository.remove(id);
-  res.sendStatus(204);
-});
+router.delete("/:id", tweetController.deleteTweet);
 
 export default router;
