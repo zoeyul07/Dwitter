d1975c4ba18f2ed8d8b58f14bc5483bf94dc1c1c
Seoyul Kim <zoeyul07@gmail.com>
2024-07-26 23:27:19 +0900

    feat: user db 연결

--- data/auth.js
+++ data/auth.js
@@ -1,15 +1,23 @@
-let users = [];
+import { db } from "../db/database.js";
 
 export async function findByUserName(userName) {
-  return users.find((user) => user.userName === userName);
+  return db
+    .execute("SELECT * FROM users WHERE userName=?", [userName])
+    .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute("SELECT * FROM users WHERE id=?", [id])
+    .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  const { userName, password, name, email, url } = user;
+  return db
+    .execute(
+      "INSERT INTO users (userName, password, name, email, url) VALUES(?,?,?,?,?)",
+      [userName, password, name, email, url]
+    )
+    .then((result) => result[0].insertId);
 }
